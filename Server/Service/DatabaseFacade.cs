5827513ea077b3764c3b5b291e086605a8427034
Nousla <sirtheorist@gmail.com>
2015-10-20 13:20:25 +0200

    Added server service and database facade, again...

---
+++ Server/Service/DatabaseFacade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Services
+{
+    public class DatabaseFacade
+    {
+        public static readonly DatabaseFacade Instance = new DatabaseFacade();
+
+        private DatabaseFacade() { }
+
+        public bool Verify(byte[] username, byte[] password)
+        {
+            return true;
+        }
+    }
+}
---
+++ Server/Service/ServerService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Services
+{
+    public class ServerService : IServerService
+    {
+        public bool Login(byte[] username, byte[] password)
+        {
+            return DatabaseFacade.Instance.Verify(username, password);
+        }
+    }
+}
