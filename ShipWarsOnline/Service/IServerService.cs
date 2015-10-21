133f4f3e7ab7aa59581d2be916e00b273fa04f7d
Goodfoamy <Goodfoamy@gmail.com>
2015-10-21 13:21:46 +0200

    Everythin replaced with stuff that works

--- ShipWarsOnline/Service/IServerService.cs
+++ ShipWarsOnline/Service/IServerService.cs
@@ -6,7 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace Services
+namespace Service
 {
     [ServiceContract]
     public interface IServerService
--- ShipWarsOnline/Service/ServerService.svc.cs
+++ ShipWarsOnline/Service/ServerService.svc.cs
@@ -6,7 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace Services
+namespace Service
 {
     public class ServerService : IServerService
     {
