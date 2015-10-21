3d4873867750684aa47139eac49a0020b9e3782f
Goodfoamy <Goodfoamy@gmail.com>
2015-10-21 13:45:36 +0200

    rest of files

---
+++ ShipWarsOnline/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+using Service;
+
+namespace Client
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Client" in both code and config file together.
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IWcfService> channelfactory = new ChannelFactory<IWcfService>("WcfServiceEndpoint");
+            IWcfService proxy = channelfactory.CreateChannel();
+
+            Console.WriteLine("Dette er Clienten");
+            Console.WriteLine(proxy.DoMath(4, 8));
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ ShipWarsOnline/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+using Service;
+
+namespace Server
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Server" in both code and config file together.
+    public class Server
+    {
+
+        static void Main(string[] args)
+        {
+            ServiceHost server = new ServiceHost(typeof(WcfService));
+            server.Open();
+
+            Console.WriteLine("Dette er Serveren");
+            Console.ReadLine();
+        }
+
+    }
+}
