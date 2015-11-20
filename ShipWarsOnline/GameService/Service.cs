590790c0bd198f9a3fc9a1d8cb10ec46f9425b1d
Minerr <jack.corp@hotmail.com>
2015-11-20 11:18:16 +0100

    Added placeholder methods for JoinLobby

--- ShipWarsOnline/GameService/Service.cs
+++ ShipWarsOnline/GameService/Service.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,40 @@ namespace GameService
 {
     public class Service : IService
     {
+        private List<string> lobby = new List<string>();
+
         public void DoStuff()
         {
             throw new NotImplementedException();
+        }
+
+        public void AddToLobby(string tokenID)
+        {
+            try
+            {
+                //lobby.Add(tokenID.username <-- Something that changes tokenID to username);
+            }
+            catch
+            {
+                throw new FaultException("Invalid username or token");
+            }
+        }
+
+        public void RemoveFromLobby(string tokenID)
+        {
+            try
+            {
+                //lobby.Remove(tokenID.username <-- Something that changes tokenID to username);
+            }
+            catch
+            {
+                throw new FaultException("Invalid username or token");
+            }
+        }
+
+        public List<string> GetLobby()
+        {
+            return lobby;
         }
     }
 }
